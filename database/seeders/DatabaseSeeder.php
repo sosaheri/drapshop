7ce82cf0691dc250ee3f634f2182f627a7d88b97
sosaheri <sosaheriberto2001@gmail.com>
2021-07-02 18:46:38 -0400

    feat: add dataseeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\People::factory(20)->create();
+        \App\Models\Product::factory(50)->create();
+
+
     }
 }
