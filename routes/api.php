01af223191caccf965afd45c0181af414fa76166
sosaheri <sosaheriberto2001@gmail.com>
2021-07-02 17:47:35 -0400

    feat: grouping and cleaning routes.

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\AuthController;
+use App\Http\Controllers\API\V1\ProductController;
+use App\Http\Controllers\API\V1\PeopleController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::group([], function () {
+
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::apiResource('products', ProductController::class)->only([ 'index', 'show',]);
+    Route::apiResource('people', PeopleController::class)->only([ 'index', 'show',]);
+
+
+
+});
+
+
+Route::group(['middleware' => ['auth:api']], function () {
+
+        Route::apiResource('products', ProductController::class)->except([ 'index', 'show',]);
+        Route::apiResource('people', PeopleController::class)->except([ 'index', 'show',]);
+
+
+});
+
